2c53546c70d1b1f27e82b986b76d2b486185073e
Shu Ding <g@shud.in>
2022-07-06 23:46:21 +0200

    fix URL

--- build.js
+++ build.js
@@ -1,32 +1,49 @@
 import { build } from 'esbuild'
+import fs from 'node:fs/promises'
 import flow from 'esbuild-plugin-flow'
 
-build({
-  bundle: true,
-  sourcemap: false,
-  format: 'esm',
-  target: 'esnext',
-  loader: {
-    '.js': 'ts',
-  },
-  entryPoints: ['./entry/index.js'],
-  outfile: './dist/entry.js',
-  external: ['*.wasm', '*.ttf'],
-  minify: true,
-  plugins: [flow(/\.js$/)],
-})
+async function start() {
+  await build({
+    bundle: true,
+    sourcemap: false,
+    format: 'esm',
+    target: 'esnext',
+    loader: {
+      '.js': 'ts',
+    },
+    entryPoints: ['./entry/index.js'],
+    outfile: './dist/entry.js',
+    external: ['*.wasm'],
+    minify: true,
+    plugins: [flow(/\.js$/)],
+  })
 
-build({
-  bundle: true,
-  sourcemap: false,
-  format: 'esm',
-  target: 'esnext',
-  loader: {
-    '.js': 'ts',
-  },
-  entryPoints: ['./index.js'],
-  outfile: './dist/index.js',
-  external: ['*.wasm', '*.ttf', './entry.js', './yoga.mjs'],
-  minify: true,
-  plugins: [flow(/\.js$/)],
-})
+  await build({
+    bundle: true,
+    sourcemap: false,
+    format: 'esm',
+    target: 'esnext',
+    loader: {
+      '.js': 'ts',
+    },
+    entryPoints: ['./index.js'],
+    outfile: './dist/index.js',
+    external: ['*.wasm', './entry.js', './yoga.mjs'],
+    minify: true,
+    plugins: [flow(/\.js$/)],
+  })
+
+  // Here a trick to avoid initializing the URL for the wasm file. Doing this
+  // because I couldn't find a way to avoid it with emcc.
+  // Related issue: https://github.com/emscripten-core/emscripten/issues/12184
+  const yoga = await fs.readFile('./dist/yoga.mjs', 'utf8')
+  await fs.writeFile(
+    './dist/yoga.mjs',
+    yoga.replace(
+      'wasmBinaryFile=new URL("yoga.wasm",import.meta.url).toString()',
+      ''
+    )
+  )
+}
+
+start()
