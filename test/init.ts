bed046a8d79e341edc401e9c494917a8a14d97f7
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-01-31 12:54:14 +0300

    fix import

--- test/init.ts
+++ test/init.ts
@@ -2,7 +2,8 @@ import { readFile } from "node:fs/promises";
 import process from "node:process";
 
 import initWasm from "..";
-import initAsm from "../asm";
+// for some reason Vite doesn't use export maps here, and resolves to source code
+import initAsm from "../dist/asm";
 
 export const Yoga = process.env.ASM
   ? initAsm()
