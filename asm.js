ed61aa83586dda423f65500f4240b6ed63375dac
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-02-21 10:40:02 +0300

    Reexport constants (#17)
    * export yoga constants from package
    
    * add tests
    
    * test asm and wasm entries

--- asm.js
+++ asm.js
@@ -1,6 +1,8 @@
 import wrapAsm from "./yoga/javascript/src_js/wrapAsm.js";
 import yoga from "./tmp/yoga-asm.mjs";
 
+export * from "./yoga/javascript/src_js/generated/YGEnums.js";
+
 export default function () {
   return wrapAsm(yoga());
 }
