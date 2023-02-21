155eeca394c7358f97d9b8ddc6bbb037141b7f82
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-02-21 10:28:16 +0300

    build everything with rollup (#18)
    * test rollup
    
    * revert test changes
    
    * run terser
    
    * create dist folder before making a copy

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import { copyFile, mkdir } from "node:fs/promises";
+import terser from "@rollup/plugin-terser";
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+
+// copy wasm file
+await mkdir("./dist/generated/", { recursive: true });
+await copyFile("./tmp/yoga.wasm", "./dist/yoga.wasm");
+
+// copy d.ts files
+await copyFile("./yoga/javascript/src_js/wrapAsm.d.ts", "./dist/wrapAsm.d.ts");
+await copyFile(
+  "./yoga/javascript/src_js/generated/YGEnums.d.ts",
+  "./dist/generated/YGEnums.d.ts"
+);
+await copyFile("./asm.d.ts", "./dist/asm.d.ts");
+
+export default [
+  {
+    input: ["asm.js", "index.js"],
+    output: {
+      dir: "dist",
+      format: "esm",
+    },
+    plugins: [
+      nodeResolve(),
+      commonjs({
+        esmExternals: true,
+      }),
+      terser({ compress: { passes: 2 } })
+    ],
+  },
+];
