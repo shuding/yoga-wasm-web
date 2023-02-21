d1b14e7cb1f0fb31362149cc42abd365d84c0c6b
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-02-21 10:10:20 +0300

    revert test changes

--- asm.js
+++ asm.js
@@ -1,8 +1,6 @@
 import wrapAsm from "./yoga/javascript/src_js/wrapAsm.js";
 import yoga from "./tmp/yoga-asm.mjs";
 
-export { ALIGN_AUTO } from "./yoga/javascript/src_js/generated/YGEnums.js";
-
 export default function () {
   return wrapAsm(yoga());
 }
